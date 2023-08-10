56d996c28bcd4ee1124b1a08ce3e47bfc9b4494d
huangchao <2637833054@qq.com>
2023-08-10 16:25:59 +0800

    新增inquirer进行脚手架命令选择

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import { Command } from 'commander'
+import inquirer from 'inquirer'
+
+const InitPrompts = [
+    {
+        type: 'list',
+        name: 'type',
+        message: '模板类型',
+        choices: ['vue', 'react', 'flutter'],
+    },
+]
 
 const program = new Command()
 program.version('1.0.0')
@@ -6,8 +16,10 @@ program.version('1.0.0')
 program
     .command('init <name>')
     .description('新建项目')
-    .action((name: string) => {
+    .action(async (name: string) => {
         console.log('测试一下init命令', name)
+        const initOptions = await inquirer.prompt(InitPrompts)
+        console.log('initOptions', initOptions)
     })
 
 program.parse()
