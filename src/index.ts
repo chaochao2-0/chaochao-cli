9490a11dd894c754adf47ed5d64c036e7a005865
huangchao <2637833054@qq.com>
2023-08-10 15:26:51 +0800

    新增commander进行命令配置

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-const aa = 'hello world'
-let bb = {
-    asd: 123,
-    123: 123,
-}
-console.log(aa)
+import { Command } from 'commander'
+
+const program = new Command()
+program.version('1.0.0')
+
+program
+    .command('init <name>')
+    .description('新建项目')
+    .action((name: string) => {
+        console.log('测试一下init命令', name)
+    })
+
+program.parse()
